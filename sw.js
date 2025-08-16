984aeeeb422c1d9a868cb9785b91601717a11b87
PeteyS <87645352+PeteyS@users.noreply.github.com>
2025-08-16 19:51:08 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('tsn-v1').then(c => c.addAll(['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-180.png'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
